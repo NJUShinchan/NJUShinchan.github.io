90e3edea601dbe0f6b3136302dbf5b5734712c62
231250185 <231250185@nju.edu.cn>
2025-07-10 10:57:30 +0800

    Site updated: 2025-07-10 10:57:29

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -1,13 +1,14 @@
+
 var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        $('[rel="icon"]').attr('href', "E:\\Desktop\\博客\\MyBlog\\themes\\particlex\\source\\images\\avatar.jpg");
         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/img/trhx2.png");
+        $('[rel="icon"]').attr('href', "E:\\Desktop\\博客\\MyBlog\\themes\\particlex\\source\\images\\avatar.jpg");
         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
